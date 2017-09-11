55373f4f5f48768f0f0c370d082667cb4e29f500
Aditya Sharma <aditya63@Adityas-MacBook-Air.local>
2017-09-11 08:16:03 -0700

    client side validation

--- src/components/pages/HomePage.js
+++ src/components/pages/HomePage.js
@@ -1,11 +1,16 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
+
 const HomePage = () => (
   <div>
-    <h1>Home Page </h1>
-    <Link to="/login">Login</Link>
+    <h1>Home Page</h1>
+      <Link to="/login">Login</Link> or <Link to="/signup">Sign Up</Link>
   </div>
+
 );
+
+
 
 export default HomePage;
